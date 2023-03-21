6c8f07b3a77cbbd9b3ef0304ab1062fe55ae2447
Anna Zharova <a.zharova.muctr@gmail.com>
2023-03-21 14:50:19 +0300

    step 3, trouble with value in username

--- frontend/src/routes/LoginPage.jsx
+++ frontend/src/routes/LoginPage.jsx
@@ -1,9 +1,9 @@
 import { useFormik } from 'formik';
 import axios from 'axios';
-import React, { useRef } from 'react';
+import React, { useRef, useState, useEffect } from 'react';
 import * as yup from 'yup';
 import {
-  Button, Form, FloatingLabel,
+  Button, Form,
 } from 'react-bootstrap';
 import { Link, useNavigate } from 'react-router-dom';
 import { toast } from 'react-toastify';
@@ -16,39 +16,49 @@ const validationSchema = yup.object().shape({
   password: yup.string().trim(),
 });
 
-const LoginPage = () => {
-  const navigate = useNavigate();
+const useSubmit = (setAuthFailed) => {
   const { logIn } = useAuth();
+  const navigate = useNavigate();
+  // const inputRef = useRef();
+  return async (values) => {
+    setAuthFailed(false);
+    try {
+      const { data } = await axios.post(routes.login, values);
+      if (data.token) {
+        const user = { token: data.token, username: data.username };
+        logIn(user);
+        localStorage.setItem('token', JSON.stringify(data.token));
+        navigate(routes.homePage);
+      }
+    } catch (error) {
+      if (!error.isAxiosError) {
+        toast.error('Неизвестная ошибка');
+        console.error(error);
+      }
+      if (error.isAxiosError && error.response?.status === 401) {
+        setAuthFailed(true);
+        // inputRef.current.select();
+      } else {
+        toast.error('Ошибка сети');
+      }
+    }
+  };
+};
+const LoginPage = () => {
+  const [authFailed, setAuthFailed] = useState(false);
   const inputRef = useRef();
+
+  useEffect(() => {
+    inputRef.current.focus();
+  }, []);
+
   const formik = useFormik({
     initialValues: {
       username: '',
       password: '',
     },
     validationSchema,
-    onSubmit: async (values, actions) => {
-      try {
-        const { data } = await axios.post(routes.login, values);
-        if (data.token) {
-          const user = { token: data.token, username: data.username };
-          // localStorage.setItem('token', data.token);
-          logIn(user);
-          navigate(routes.homePage);
-        }
-      } catch (error) {
-        console.error(error);
-        if (!error.isAxiosError) {
-          toast.error('Неизвестная ошибка');
-          return;
-        }
-        if (error.responce?.status === 401) {
-          actions.setFieldError('authentification', 'auth');
-          inputRef.current.select();
-        } else {
-          toast.error('Ошибка сети');
-        }
-      }
-    },
+    onSubmit: useSubmit(setAuthFailed),
   });
 
   return (
@@ -66,8 +76,8 @@ const LoginPage = () => {
               </div>
               <Form className="col-12 col-md-6 mt-3 mt-mb-0" onSubmit={formik.handleSubmit}>
                 <h1 className="text-center mb-4">Войти</h1>
-                <Form.Group className="form-floating mb-3" controlId="username">
-                  <FloatingLabel className={formik.values.username && 'filled'} controlId="floatingUsername">
+                <fieldset disabled={formik.isSubmitting}>
+                  <Form.Group className="form-floating mb-3" controlId="username">
                     <Form.Control
                       name="usernsme"
                       type="username"
@@ -75,31 +85,31 @@ const LoginPage = () => {
                       autoComplete="username"
                       required
                       autoFocus
-                      isInvalid={formik.errors.authentification}
+                      isInvalid={authFailed || (formik.touched.username && formik.errors.username)}
                       onChange={formik.handleChange}
-                      // value={formik.values.username}
+                      value={formik.values.username}
                       ref={inputRef}
                     />
-                  </FloatingLabel>
-                </Form.Group>
-                <Form.Group className="form-floating mb-4" controlId="password">
-                  <FloatingLabel className={formik.values.password && 'filled'} controlId="floatingPassword">
+                    <Form.Label className="form-label">Ваш ник</Form.Label>
+                  </Form.Group>
+                  <Form.Group className="form-floating mb-4" controlId="password">
                     <Form.Control
                       name="password"
                       type="password"
                       autoComplete="current-password"
                       placeholder="Пароль"
                       required
-                      isInvalid={formik.errors.authentification}
+                      isInvalid={authFailed || (formik.touched.password && formik.errors.password)}
                       onChange={formik.handleChange}
                       value={formik.values.password}
                     />
+                    <Form.Label className="form-label">Пароль</Form.Label>
                     <Form.Control.Feedback type="invalid" tooltip>
-                      {formik.errors.authentication && 'Неверный логин или пароль'}
+                      Неверные имя пользователя или пароль
                     </Form.Control.Feedback>
-                  </FloatingLabel>
-                </Form.Group>
-                <Button className="w-100 mb-3" variant="outline-primary" type="submit">Войти</Button>
+                  </Form.Group>
+                  <Button className="w-100 mb-3" variant="outline-primary" type="submit">Войти</Button>
+                </fieldset>
               </Form>
             </div>
             <div className="card-footer p-4">
